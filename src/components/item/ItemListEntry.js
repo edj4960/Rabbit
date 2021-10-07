94099b980d3a0fb5a6afc58619fe2e88a4ddf885
edj4960 <evandj423@gmail.com>
2021-10-06 21:58:52 -0600

    RAB-12 Item List - Item deletion

--- src/components/item/ItemListEntry.js
+++ src/components/item/ItemListEntry.js
@@ -3,10 +3,13 @@ import { StyleSheet, TextInput } from 'react-native';
 import { Box, Text } from 'react-native-design-utility';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { focusInputWithKeyboard } from '../../utilities';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import { db } from '../../firebase';
 
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import Animated from 'react-native-reanimated';
 import CompleteItem from './CompleteItem';
+import SwipeableItem from 'react-native-swipeable-item'
 
 const ItemListEntry = ({ item, drag, storeId }) => {
   const [name, setName] = useState('');
@@ -48,30 +51,54 @@ const ItemListEntry = ({ item, drag, storeId }) => {
     }
   }, [textInputRef]);
 
-  return (
-    <TouchableOpacity
-      onLongPress={drag}
-      delayLongPress={250}
-    >
-      <Box
-        p={10}
-        w="100%"
-        h={80}
-        style={styles.itemContainer}
-        flex={true}
-        flexDirection="row"
-        alignItems="center"
+  const renderUnderlayLeft = ({ percentOpen }) => {
+    return (
+      <Animated.View
+        style={[styles.row, styles.underlayLeft, { opacity: percentOpen }]}
       >
-        <CompleteItem itemId={item.id} />
-        {
-          !isEdit &&
-          <TouchableOpacity
-            onPress={startEdit}
-          >
-            <Text>{name}</Text>
-          </TouchableOpacity>
-        }
-        {/* <Box> */}
+        <TouchableOpacity
+          style={styles.deleteButton}
+          onPress={deleteItem}
+        >
+          <FontAwesomeIcon
+            icon={faTrashAlt}
+            size={20}
+          />
+        </TouchableOpacity>
+      </Animated.View>
+    )
+  }
+
+  return (
+    <SwipeableItem
+      overSwipe={20}
+      snapPointsLeft={[75]}
+      key={item.id}
+      item={item}
+      renderUnderlayLeft={renderUnderlayLeft}
+    >
+      <TouchableOpacity
+        onLongPress={drag}
+        delayLongPress={400}
+      >
+        <Box
+          p={10}
+          w="100%"
+          h={80}
+          style={styles.itemContainer}
+          flex={true}
+          flexDirection="row"
+          alignItems="center"
+        >
+          <CompleteItem itemId={item.id} />
+          {
+            !isEdit &&
+            <TouchableOpacity
+              onPress={startEdit}
+            >
+              <Text>{name}</Text>
+            </TouchableOpacity>
+          }
           <TextInput
             ref={textInputRef}
             style={{
@@ -84,9 +111,9 @@ const ItemListEntry = ({ item, drag, storeId }) => {
             onEndEditing={submitName}
             onSubmitEditing={submitName}
           />        
-        {/* </Box> */}
-      </Box>
-    </TouchableOpacity>
+        </Box>
+      </TouchableOpacity>
+    </SwipeableItem>
   )
 }
 
@@ -96,6 +123,26 @@ const styles = StyleSheet.create({
   },
   itemTextInput: {
     height: 10
+  },
+  underlayLeft: {
+    flex: 1,
+    backgroundColor: 'tomato',
+    justifyContent: 'flex-end',
+  },
+  row: {
+    flexDirection: 'row',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  deleteButton: {
+    width: 75,
+    height: '100%',
+    flex: 1,
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'grey'
   }
 })
 
